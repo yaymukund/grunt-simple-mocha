c47ece16faed4abd376a82ee532fd94af39fa519
Mukund Lakshman <yaymukund@gmail.com>
2012-09-02 15:17:47 -0700

    Added the task using grunt init:gruntplugin

---
+++ tasks/simple-mocha.js
@@ -0,0 +1,27 @@
+/*
+ * grunt-simple-mocha
+ * https://github.com/yaymukund/grunt-simple-mocha
+ *
+ * Copyright (c) 2012 Mukund Lakshman
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  var path = require('path'),
+      Mocha = require('mocha');
+
+  grunt.registerTask('mocha', 'Run tests with mocha', function() {
+
+    var filepaths = grunt.file.expandFiles(this.file.src);
+    grunt.file.clearRequireCache(filepaths);
+
+    var paths = filepaths.map(path.resolve),
+        options = this.data.options || {},
+        mocha_instance = new Mocha(options);
+
+    paths.map(mocha_instance.addFile.bind(mocha_instance));
+    mocha_instance.run(this.async());
+
+  });
+};
